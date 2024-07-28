ee5520a37f48b84268ea3fdfc23a562df3dc5b5b
Jamal-009 <jamaljamali11220@gmail.com>
2024-07-28 18:21:15 +0500

    Conversion of data

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 0
+let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
